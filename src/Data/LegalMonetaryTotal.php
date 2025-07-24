078589425c58dbb393cd1244b61bb9cf64a899d3
Raditz Farhan <raditzfarhan@gmail.com>
2025-07-24 14:27:48 +0800

    Sort parameter in LegalMonetaryTotal

--- src/Data/LegalMonetaryTotal.php
+++ src/Data/LegalMonetaryTotal.php
@@ -7,13 +7,13 @@ use Laraditz\MyInvois\Enums\XMLNS;
 class LegalMonetaryTotal extends AbstractData
 {
     public function __construct(
+        public Data|Money|null $LineExtensionAmount = null,
         public Data|Money $TaxExclusiveAmount,
         public Data|Money $TaxInclusiveAmount,
-        public Data|Money $PayableAmount,
-        public Data|Money|null $LineExtensionAmount = null,
         public Data|Money|null $AllowanceTotalAmount = null,
         public Data|Money|null $ChargeTotalAmount = null,
         public Data|Money|null $PayableRoundingAmount = null,
+        public Data|Money $PayableAmount,
     ) {
     }
 
