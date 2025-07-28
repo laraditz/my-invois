ef1cce07790116911265a07a8e8ab9d8feae8568
Raditz Farhan <raditzfarhan@gmail.com>
2025-07-28 14:46:08 +0800

    Update LegalMonetaryTotal data

--- src/Data/LegalMonetaryTotal.php
+++ src/Data/LegalMonetaryTotal.php
@@ -8,12 +8,12 @@ class LegalMonetaryTotal extends AbstractData
 {
     public function __construct(
         public Data|Money|null $LineExtensionAmount = null,
-        public Data|Money $TaxExclusiveAmount,
-        public Data|Money $TaxInclusiveAmount,
+        public Data|Money|null $TaxExclusiveAmount = null,
+        public Data|Money|null $TaxInclusiveAmount = null,
         public Data|Money|null $AllowanceTotalAmount = null,
         public Data|Money|null $ChargeTotalAmount = null,
         public Data|Money|null $PayableRoundingAmount = null,
-        public Data|Money $PayableAmount,
+        public Data|Money|null $PayableAmount = null,
     ) {
     }
 
